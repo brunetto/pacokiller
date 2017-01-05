b43d4469b356bdc9799e357ebc81da1866252709
Brunetto Ziosi <brunetto.ziosi@pixartprinting.com>
2017-01-05 11:28:50 +0100

    refactoring

--- main.go
+++ main.go
@@ -1,60 +1,16 @@
 package main
 
 import (
-	ps "github.com/mitchellh/go-ps"
-	"fmt"
+	"github.com/brunetto/goutils/system"
 	"log"
-	"strings"
-	"syscall"
-	"time"
 )
 
-func main () {
-	var (
-		processes Processes
-		procs	Processes
-		err error
-	)
+func main() {
+	var err error
 
-	for {
-		processes, err = ps.Processes()
-		if err != nil {
-			log.Fatal("Can't retrieve processes list: ", err)
-		}
-		procs = processes.FindProcessByName("parentalcontrols")
-		//procs.Print()
+	err = system.MonitorAndKill("parentalcontrol")
 
-		if len(procs) == 1 {
-			syscall.Kill(procs[0].Pid(), syscall.SIGKILL)
-		}
-
-		time.Sleep(20*time.Second)
+	if err != nil {
+		log.Fatal(err)
 	}
-}
-
-type Processes []ps.Process
-type Process struct {
-	ps.Process
-}
-
-func (p *Processes) FindProcessByName (searchString string) Processes {
-	var procs Processes
-	for _, proc := range *p {
-		if strings.Contains(strings.ToLower(proc.Executable()), strings.ToLower(searchString)) {
-			procs = append(procs, proc)
-		}
-	}
-	return procs
-}
-
-func (p *Processes) Print () {
-	fmt.Println("PID\t\tExecutable name")
-	fmt.Println("===\t\t===============")
-	for _, proc := range *p {
-		fmt.Println(proc.Pid(), "\t\t", proc.Executable())
-	}
-}
-
-func (p *Process) Kill() () {
-	syscall.Kill(p.Process.Pid(), syscall.SIGKILL)
 }
